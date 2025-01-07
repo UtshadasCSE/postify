a4b71744f5907ec2678334817be42db959494551
UtshadasCSE <utshachandradas@gmail.com>
2025-01-07 23:23:56 +0600

    Clerk user successfully store in database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="p-3 flex gap-2">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button> Hello Signin Now</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <Button variant={"outline"}> Hello Signin Now</Button>
-
-      <ModeToggle />
+    <div className="m-4">
+      <h2>Home page content</h2>
     </div>
   );
 }
---
+++ src/components/DesktopNavbar.tsx
@@ -0,0 +1,57 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { BellIcon, HomeIcon, UserIcon } from "lucide-react";
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import ModeToggle from "./ModeToggle";
+
+const DesktopNavbar = async () => {
+  const user = await currentUser();
+  // console.log(user);
+
+  return (
+    <div>
+      {" "}
+      <div className="hidden md:flex items-center space-x-4">
+        <ModeToggle />
+
+        <Button variant="ghost" className="flex items-center gap-2" asChild>
+          <Link href="/">
+            <HomeIcon className="w-4 h-4" />
+            <span className="hidden lg:inline">Home</span>
+          </Link>
+        </Button>
+
+        {user ? (
+          <>
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link href="/notifications">
+                <BellIcon className="w-4 h-4" />
+                <span className="hidden lg:inline">Notifications</span>
+              </Link>
+            </Button>
+            <Button variant="ghost" className="flex items-center gap-2" asChild>
+              <Link
+                href={`/profile/${
+                  user.username ??
+                  user.emailAddresses[0].emailAddress.split("@")[0]
+                }`}
+              >
+                <UserIcon className="w-4 h-4" />
+                <span className="hidden lg:inline">Profile</span>
+              </Link>
+            </Button>
+            <UserButton />
+          </>
+        ) : (
+          <SignInButton mode="modal">
+            <Button variant="default">Sign In</Button>
+          </SignInButton>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default DesktopNavbar;
